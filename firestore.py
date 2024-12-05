7e5e829507c5fa8ee940797d1ef503fdb4d2db4d
LaFlowBy <weidnerflo@gmail.com>
2024-12-05 16:46:48 +0100

    logging

--- firestore.py
+++ firestore.py
@@ -3,7 +3,6 @@ from firebase_admin import firestore
 class FirestoreDatabase:
     def __init__(self, credentials_path=None):
         # Initialize Firestore client
-        credentials_path = os.path.expanduser(credentials_path) if credentials_path else None
         self.db = firestore.Client.from_service_account_json(credentials_path) if credentials_path else firestore.Client()
         self.PLANTS_COLLECTION = 'plants'
         self.DATA_COLLECTION = 'data'
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 """Main Module"""
 import sys
 import time
+import logging
 import Adafruit_ADS1x15
 from firestore import FirestoreDatabase
 
+# Configure logging to log to a file
+logging.basicConfig(filename='/home/florian/main.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 try:
-    SERVICE_ACCOUNT_PATH = '~/Repos/PlantWater/serviceAccountKey.json'
     adc = Adafruit_ADS1x15.ADS1115()
+    logging.info(sys.argv[1])
     db = FirestoreDatabase(sys.argv[1] if len(sys.argv) > 1 else None)
 
     GAIN = 1
@@ -14,11 +18,14 @@ try:
     plant = db.findPlantByName(NAME)
     if plant is None:
         plant = db.addPlant(NAME, 'Succulent')
+        logging.info(f"Added new plant: {NAME}")
 
     while True:
         data_value = adc.read_adc(3, gain=GAIN)
         db.addDataToPlant(plant, data_value)
-        print(data_value)
+        logging.info(f"Data value: {data_value}")
         time.sleep(10)
 except KeyboardInterrupt:
-    print("Exit")
+    logging.info("Exit")
+except Exception as e:
+    logging.error(f"An error occurred: {e}")
