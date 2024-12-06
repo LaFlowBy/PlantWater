d6dd821a914334437880d05f1e2bb1f991922eba
FlowBYRN <weidnerflo@gmail.com>
2024-12-06 01:49:29 +0100

    change on date

--- src/app/angular-firebase-template/src/app/main-page/main-page.component.ts
+++ src/app/angular-firebase-template/src/app/main-page/main-page.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import Fireworks from 'fireworks-js';
 
 @Component({
   selector: 'app-main-page',
-  imports: [],
+  imports: [CommonModule ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss',
   standalone: true
@@ -26,6 +27,12 @@ export class MainPageComponent  implements OnInit, AfterViewInit{
       this.fireworks = new Fireworks(container);
       this.fireworks.start();
     }
+  }
+
+  get isBirthday(): boolean {
+    const today = new Date();
+    const birthDate = new Date(2024, 12, 7);
+    return today.getTime() === birthDate.getTime();
   }
 
   ngOnDestroy(): void {
