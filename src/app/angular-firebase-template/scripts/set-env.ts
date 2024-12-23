736f56b88e09bb863932e3bdb40ff8f2dccad630
FlowBYRN <weidnerflo@gmail.com>
2024-12-23 16:28:47 +0100

    create env variable for api key

---
+++ src/app/angular-firebase-template/scripts/set-env.ts
@@ -0,0 +1,33 @@
+// filepath: /c:/WS/PlantWater/scripts/set-env.ts
+const { writeFile } = require('fs');
+const { argv } = require('yargs');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
+const targetPath = `./src/environments/environment.ts`;
+
+// Define environment variables to be injected
+const envConfigFile = `
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: '${process.env["PLANTWATER_FIREBASE_API_KEY"]}',
+    authDomain: 'plantwater-f986c.firebaseapp.com',
+    projectId: 'plantwater-f986c',
+    storageBucket: 'plantwater-f986c.firebasestorage.app',
+    messagingSenderId: '873827710935',
+    appId: '1:873827710935:web:d7170b165186550ff7fd92'
+  }
+};
+`;
+
+// Write the environment variables to the environment.ts file
+writeFile(targetPath, envConfigFile, function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Environment variables written to ${targetPath}`);
+  }
+});
--- src/app/angular-firebase-template/src/app/app.config.ts
+++ src/app/angular-firebase-template/src/app/app.config.ts
@@ -4,12 +4,12 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { routes } from './app.routes';
-import { firebaseConfig } from '../environments/environment';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideFirestore(() => getFirestore()),
     provideAuth(() => getAuth()),
     provideRouter(routes),
